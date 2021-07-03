3faba14a4f4b1ddcf4d6b9e28ed6ccfa8480a0e7
Lucas Gutierrez <lucas.gutierrez@hotmail.com.ar>
2021-07-02 23:59:12 -0300

    en wallets se muestran las transacciones realizadas

--- modules/wallet/components/CardWallet.js
+++ modules/wallet/components/CardWallet.js
@@ -8,7 +8,10 @@ import {
   Button,
   Form,
   FormGroup,
+  Table
 } from "react-bootstrap";
+
+import Image from "next/image";
 
 const CardWallet = ({
   wallets,
@@ -21,6 +24,7 @@ const CardWallet = ({
     <Container>
       <Row>
         {Object.values(wallets).map((wallet) => (
+
           <Col lg={4} md={6} sm={6} xs={12} key={wallet.id}>
             <Card className="mt-2 mr-1">
               <Card.Body>
@@ -29,7 +33,37 @@ const CardWallet = ({
                     <Card.Title>
                       <Link href={"/wallets/" + wallet.id}>{wallet.name}</Link>
                     </Card.Title>
-                    <Card.Text>Criptos en esta cartera</Card.Text>
+                    {/* {console.log(Object.values(wallet.transactions))} */}
+                    <Table bordered size="sm" variant="dark" className="text-center">
+                      <thead>
+                        <tr>
+                          <th>Type</th>
+                          <th>Coin</th>
+                          <th>Price</th>
+                          <th>Quantity</th>
+                          <th>Total</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <>
+
+                          {Object.values(wallet.transactions).map((trans) => (
+                            <> <tr>
+                              <td>{trans.transactionType}</td>
+                              <td>{trans.coin}</td>
+                              <td>{trans.price}</td>
+                              <td>{trans.qty}</td>
+                              <td>{trans.total}</td>  </tr>
+                            </>
+                          ))}
+
+
+                        </>
+
+
+                      </tbody>
+                    </Table>
+
                   </>
                 ) : (
                   <Form onSubmit={(e) => submitEdits(e, wallet.id)}>
